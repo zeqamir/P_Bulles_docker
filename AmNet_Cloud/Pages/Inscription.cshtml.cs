00d29251c3c666bfd9d0957a73860d7a92d52ad9
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-03-26 15:39:27 +0100

    init project
    docker compose, ...

---
+++ AmNet_Cloud/Pages/Inscription.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace AmNet_Cloud.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
